fd40b84bc895a6120ff64756e71c44e58a78d086
Michael <michael@Administrators-MacBook-Pro.local>
2020-07-01 09:46:07 -0400

    change validation on name

--- cmd/repo.go
+++ cmd/repo.go
@@ -16,7 +16,7 @@ func promptReponame() (string, error) {
 
 	validate := func(input string) error {
 		if len(input) < 5 {
-			return errors.New("Service name must have more than 5 characters")
+			return errors.New("Project name must have more than 5 characters")
 		}
 		return nil
 	}
