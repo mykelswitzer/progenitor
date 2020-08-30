6b6aa6582ea0eeff31d5d1d9d2ee527f4b100e2a
Clay Raynor <cd.raynor@gmail.com>
2020-08-30 14:59:48 -0400

    Add initTerraform() function

--- internal/terraform/terraform.go
+++ internal/terraform/terraform.go
@@ -32,3 +32,16 @@ func getTerraformVersion() ([]byte, error) {
     pattern := regexp.MustCompile(`v[0-9]\.[0-9]+\.[0-9]+`)
     return pattern.Find(out), nil
 }
+
+// initTerraform initializes the Terraform directory of the newly
+// created project repository. The var tfDir is the absolute path
+// of the Terraform directory.
+func initTerraform(tfDir string) error {
+    tf := exec.Command("terraform", "init", tfDir)
+    err := tf.Run()
+
+    if err != nil {
+        return errors.Wrap(err, "Error encountered while initializing Terraform directory!")
+    }
+    return nil
+}
--- internal/terraform/terraform_test.go
+++ internal/terraform/terraform_test.go
@@ -1,9 +1,12 @@
 package repo
 
-import "testing"
+import (
+    "os"
+    "testing"
+)
 
 // TODO: Find a way to mock this so the test can run in any environment
-// Verifies the isTerraformInstalled function doesn't return an error if
+// Verifies that the isTerraformInstalled function doesn't return an error if
 // Terraform is installed on the host running the test. Its not exactly
 // a unit test since it depends on the host running it.
 func Test_isTerraformInstalled(t *testing.T) {
@@ -14,8 +17,8 @@ func Test_isTerraformInstalled(t *testing.T) {
     }
 }
 
-// TODO: Find better method of testing that doesn't hard code version
-// Verifies the getTerraformVersion function returns the installed
+// TODO: Find a better method of testing that doesn't hard code version
+// Verifies that the getTerraformVersion function returns the installed
 // version of Terraform. Its not exactly a unit test since it depends on
 // on the host running it.
 func Test_getTerraformVersion(t *testing.T)  {
@@ -32,5 +35,21 @@ func Test_getTerraformVersion(t *testing.T)  {
     if string(version) != "v0.12.29" {
        t.Log(string(version))
        t.Fatal("Incorrect version returned!")
+    }
+}
+
+// TODO: Find a way to mock this so the test can run in any environment
+// Verifies the initTerraform function successfully executes the command
+// `terraform init` inside the Terraform directory of the newly create
+// repository
+func Test_initTerraform(t *testing.T) {
+    tfDir := os.Getenv("TF_DIR")
+    if len(tfDir) < 1 {
+        t.Fatal("Aborting test, the environment variable TF_DIR not set!")
+    }
+
+    err := initTerraform(tfDir)
+    if err != nil {
+        t.Fatal("Unexpected error encountered!")
     }
 }
