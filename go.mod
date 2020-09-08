6a2908c8b30c4734fc5893e3b33d0f39810df451
Clay Raynor <cd.raynor@gmail.com>
2020-09-08 13:55:22 -0400

    Use terraform-exec package for running CLI commands

--- go.mod
+++ go.mod
@@ -7,8 +7,8 @@ require (
 	github.com/caring/go-packages v1.6.2
 	github.com/go-git/go-git/v5 v5.1.0
 	github.com/google/go-github/v32 v32.0.0
+	github.com/hashicorp/terraform-exec v0.8.0
 	github.com/manifoldco/promptui v0.7.0
-	github.com/pkg/errors v0.9.1
 	github.com/posener/gitfs v1.2.1
 	github.com/spf13/afero v1.3.1
 	github.com/spf13/cast v1.3.1
--- internal/terraform/terraform.go
+++ internal/terraform/terraform.go
@@ -1,6 +1,7 @@
 package terraform
 
 import (
+    "context"
     "log"
     "os"
     "os/exec"
@@ -9,19 +10,20 @@ import (
 )
 
 import "github.com/caring/go-packages/pkg/errors"
+import "github.com/hashicorp/terraform-exec/tfexec"
 
 // TODO: Find a better way to organize these functions. Perhaps make them part of an interface
 
-// isTerraformInstalled checks if Terraform is installed by searching for it
-// in the directories named by the PATH environment variable. If its not found
-// an error is returned, otherwise nil is returned.
-func isTerraformInstalled() error {
-    _, err := exec.LookPath("terraform")
+// isTerraformInstalled checks if Terraform is installed by searching for it in the directories named
+//by the PATH environment variable. If found the path of the Terraform executable is returned. If it's
+// not found, an error is returned.
+func isTerraformInstalled() (string, error) {
+    path, err := exec.LookPath("terraform")
 
     if err != nil {
-        return errors.Wrap(err, "Terraform not found in PATH")
+        return "", errors.Wrap(err, "Terraform not found in PATH")
     }
-    return nil
+    return path, nil
 }
 // TODO: Add timeout and better OS signal handling
 // getVersion executes the command `terraform version` and
@@ -148,32 +150,36 @@ func apply(tfDir string) error {
 // TfRun chains together all the steps to run the newly generated project's Terraform
 func TfRun(tfDir string) error {
     awsEnvs := []string{"caring-prod", "caring-stg", "caring-dev"}
-
-    if isTerraformInstalled() != nil {
+    installedPath, err := isTerraformInstalled()
+    if err != nil {
         return errors.New("Could not find Terraform installed on PATH")
     }
 
+    tf, err := tfexec.NewTerraform(tfDir, installedPath)
     log.Println("Initializing Terraform directory!")
-    err := initTf(tfDir)
+    if tf == nil {
+        return errors.New("Error encountered when initializing Terraform!")
+    }
+    err = tf.Init(context.Background())
     if err != nil {
         return err
     }
 
     for _, s := range awsEnvs {
        log.Println("Creating Terraform workspace: ", s)
-       err := newWorkspace(tfDir, s)
+       err := tf.WorkspaceNew(context.Background(), s)
        if err != nil {
            return err
        }
     }
 
     log.Println("Applying Terraform plan to 'caring-dev' environment")
-    err = selectWorkspace(tfDir, "caring-dev")
+    err = tf.WorkspaceSelect(context.Background(), awsEnvs[2])
     if err != nil {
         return err
     }
 
-    err = apply(tfDir)
+    err = tf.Apply(context.Background())
     if err != nil {
         return err
     }
