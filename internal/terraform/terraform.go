88fdaf9d1d701b4b1445c1bbb4ecad6cd0a23928
Michael Switzer <mswitzer@ip-192-168-1-158.ec2.internal>
2020-10-13 18:45:49 -0400

    minor cleanup

--- internal/terraform/terraform.go
+++ internal/terraform/terraform.go
@@ -9,14 +9,11 @@ import (
 import "github.com/caring/go-packages/pkg/errors"
 import "github.com/hashicorp/terraform-exec/tfexec"
 
-// TODO: Find a better way to organize these functions. Perhaps make them part of an interface
-
-// isTerraformInstalled checks if Terraform is installed by searching for it in the directories named
-//by the PATH environment variable. If found the path of the Terraform executable is returned. If it's
-// not found, an error is returned.
-func isTerraformInstalled() (string, error) {
+// isInstalled checks if Terraform is installed by searching for it in the directories named
+// by the PATH environment variable. If found the path of the Terraform executable is returned.
+// Otherwise, an error is returned.
+func isInstalled() (string, error) {
 	path, err := exec.LookPath("terraform")
-
 	if err != nil {
 		return "", errors.Wrap(err, "Terraform not found in PATH")
 	}
@@ -24,9 +21,9 @@ func isTerraformInstalled() (string, error) {
 }
 
 // TfRun chains together all the steps to run the newly generated project's Terraform
-func TfRun(tfDir string) error {
+func Run(tfDir string) error {
 	awsEnvs := []string{"caring-prod", "caring-stg", "caring-dev"}
-	installedPath, err := isTerraformInstalled()
+	installedPath, err := isInstalled()
 	if err != nil {
 		return errors.New("Could not find Terraform installed on PATH")
 	}
