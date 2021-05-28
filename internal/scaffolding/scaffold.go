dedb3500ca734831ee2d146a375814f0074b66f9
Clay Raynor <cd.raynor@gmail.com>
2021-05-28 14:20:31 -0400

    Revert changes to path

--- internal/scaffolding/scaffold.go
+++ internal/scaffolding/scaffold.go
@@ -99,8 +99,7 @@ func createDirs(dirs []Dir, parent afero.Fs) error {
 // build the project files in the local directory
 func (s *Scaffold) BuildFiles(token string) error {
 
-	// base := "github.com/caring/progenitor/internal/templates"
-	base := "github.com/caring/progenitor/internal/templates@heads/PRO-72"
+	base := "github.com/caring/progenitor/internal/templates"
 	templates, err := getLatestTemplates(token, filepath.Join(base, s.TemplatePath), s.SkipTemplates, s.Fs)
 	if err != nil {
 		return err
