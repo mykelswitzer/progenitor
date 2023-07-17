517db4cbf62f5f12633fd3f705d4aec9901ee047
Michael Switzer <mykelswitzer@Michaels-MBP-2.attlocal.net>
2023-07-17 12:21:45 -0400

    typo

--- pkg/scaffold/template.go
+++ pkg/scaffold/template.go
@@ -127,8 +127,6 @@ func getDirAndParentFromPath(filePath string) (dirName string, parent string) {
 
 func mapDirs(dirs map[string][]string, filePath string) map[string][]string {
 	dirName, parent := getDirAndParentFromPath(filePath)
-	dirs[dirName] = []string{}
-
 	_, ok := dirs[parent]
 	if !ok {
 		dirs[parent] = []string{dirName}
